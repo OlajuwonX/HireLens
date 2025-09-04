e3d375147d7048f64be2158ade22203ec8251311
OlajuwonX <olasimboolajuwon@gmail.com>
2025-09-04 06:02:57 +0100

    after adding the resume data

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,8 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "../components/navbar";
+import Particles from "../components/Particles";
+import {resumes} from "../../constants";
+import ResumeCard from "../components/resume.card";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +12,39 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="relative w-full h-screen overflow-auto bg-gray-200">
+      <Navbar />
+      {/* Particles Background*/}
+      <div className="absolute inset-0 z-0">
+        <Particles
+            particleColors={["#00ff00", "#00008b", "#ff0000"]}
+            particleCount={200}
+            particleSpread={12}
+            speed={0.2}
+            particleBaseSize={130}
+            moveParticlesOnHover={true}
+            alphaParticles={false}
+            disableRotation={false}
+            className="w-full h-full"
+        />
+      </div>
+      <section className="main-section relative z-10 pt-12 pb-20 px-4">
+        <div className="page-heading text-center">
+          <h1>Monitor Your Applications and Resume Scores</h1>
+          <h2>Stay Ahead: Track Submissions and AI Insights</h2>
+        </div>
+
+          {resumes.length > 0 && (
+              <div className="resumes-section w-full max-w-7xl">
+                  {resumes.map((resume) => (
+                      <ResumeCard key={resume.id} resume={resume} />
+                  ))}
+              </div>
+          )}
+      </section>
+
+
+    </main>
+  );
 }
