0a902982ec4a2a84dd47512242466dcbad8bbeea
OlajuwonX <olasimboolajuwon@gmail.com>
2025-09-13 12:53:58 +0100

    reverted changes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -7,10 +7,10 @@ import {useEffect, useState} from "react";
 import {usePuterStore} from "../lib/puter";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "HireLens" },
-    { name: "description", content: "Analyse your resume." },
-  ];
+    return [
+        { title: "HireLens" },
+        { name: "description", content: "Analyse your resume." },
+    ];
 }
 
 export default function Home() {
@@ -41,53 +41,53 @@ export default function Home() {
     }, []);
 
     return (
-    <main className="relative w-full h-screen overflow-auto bg-gray-100">
-      <Navbar />
-      {/* Particles Background*/}
-      <div className="absolute inset-0 z-0">
-        <Particles
-            particleColors={["#00ff00", "#00008b", "#ff0000"]}
-            particleCount={200}
-            particleSpread={12}
-            speed={0.2}
-            particleBaseSize={130}
-            moveParticlesOnHover={true}
-            alphaParticles={false}
-            disableRotation={false}
-            className="w-full h-full"
-        />
-      </div>
-      <section className="main-section relative z-10 pt-12 pb-20 px-4">
-        <div className="page-heading text-center">
-          <h1 className="text-6xl md:text-4xl sm:text-xl text-gradient leading-tight tracking-[-2px] font-semibold">Monitor Your Applications and Resume Scores</h1>
-            {!loadingResumes && resumes?.length === 0 ? (
-                <h2>Upload your resume for review</h2>
-            ) : (
-                <h2>Stay Ahead: Track Submissions and AI Insights</h2>
-            )}
-        </div>
-          {loadingResumes && (
-              <div className="flex flex-col justify-center items-center">
-                  <img src="/images/resume-scan-2.gif" alt="Scanning" />
-              </div>
-          )}
+        <main className="relative w-full h-screen overflow-auto bg-gray-100">
+            <Navbar />
+            {/* Particles Background*/}
+            <div className="absolute inset-0 z-0">
+                <Particles
+                    particleColors={["#00ff00", "#00008b", "#ff0000"]}
+                    particleCount={200}
+                    particleSpread={12}
+                    speed={0.2}
+                    particleBaseSize={130}
+                    moveParticlesOnHover={true}
+                    alphaParticles={false}
+                    disableRotation={false}
+                    className="w-full h-full"
+                />
+            </div>
+            <section className="main-section relative z-10 pt-12 pb-20 px-4">
+                <div className="page-heading text-center">
+                    <h1>Monitor Your Applications and Resume Scores</h1>
+                    {!loadingResumes && resumes?.length === 0 ? (
+                        <h2>Upload your resume for review</h2>
+                    ) : (
+                        <h2>Stay Ahead: Track Submissions and AI Insights</h2>
+                    )}
+                </div>
+                {loadingResumes && (
+                    <div className="flex flex-col justify-center items-center">
+                        <img src="/images/resume-scan-2.gif" alt="Scanning" />
+                    </div>
+                )}
 
-          {!loadingResumes && resumes.length > 0 && (
-              <div className="resumes-section w-full max-w-7xl">
-                  {resumes.map((resume) => (
-                      <ResumeCard key={resume.id} resume={resume} />
-                  ))}
-              </div>
-          )}
+                {!loadingResumes && resumes.length > 0 && (
+                    <div className="resumes-section w-full max-w-7xl">
+                        {resumes.map((resume) => (
+                            <ResumeCard key={resume.id} resume={resume} />
+                        ))}
+                    </div>
+                )}
 
-          {!loadingResumes && resumes?.length === 0 && (
-              <div className="flex flex-col itemscenter justify-center mt-10 gap-4">
-                  <Link to="/upload" className="font-semibold primary-button w-fit text-xl">Upload Resume</Link>
-              </div>
-          )}
-      </section>
+                {!loadingResumes && resumes?.length === 0 && (
+                    <div className="flex flex-col itemscenter justify-center mt-10 gap-4">
+                        <Link to="/upload" className="font-semibold primary-button w-fit text-xl">Upload Resume</Link>
+                    </div>
+                )}
+            </section>
 
 
-    </main>
-  );
+        </main>
+    );
 }
